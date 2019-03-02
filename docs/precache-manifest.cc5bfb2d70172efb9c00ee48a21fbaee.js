0bef7b11b43023122d86ee17c2fad28acace2c01
Juan sanchez <juansanchez@AL-COL-02407.local>
2019-03-02 17:32:17 -0500

    testing github pages with docs/ folder

---
+++ docs/precache-manifest.cc5bfb2d70172efb9c00ee48a21fbaee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83ba62ecfb7bf658d188",
+    "url": "/static/css/main.defb02d1.chunk.css"
+  },
+  {
+    "revision": "83ba62ecfb7bf658d188",
+    "url": "/static/js/main.83ba62ec.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "27ffa7a4598226c2b820",
+    "url": "/static/js/2.27ffa7a4.chunk.js"
+  },
+  {
+    "revision": "caa08e75af0d3fc58ff4ff216c5468f3",
+    "url": "/index.html"
+  }
+];
